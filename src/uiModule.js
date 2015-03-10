25ad0b2f05f964aa757dbbdd7ef835aa841a5106
Thomas Brekelmans <thomas.brekelmans@liones.nl>
2015-03-10 13:56:45 +0100

    Fix dependencies.

--- src/uiModule.js
+++ src/uiModule.js
@@ -1,15 +1,30 @@
 define([
 	'angular',
 
+	'fontoxml-ui-buttons/uiModule',
+	'fontoxml-ui-modal/uiModule',
+	'fontoxml-ui-regions/uiModule',
+	'fontoxml-ui-panels/uiModule',
+
 	'./SpecialCharacterController'
 ], function (
 	angular,
+
+	uiButtonsUiModule,
+	uiModalUiModule,
+	uiRegionsUiModule,
+	uiPanelsUiModule,
 
 	SpecialCharacterController
 ) {
 	'use strict';
 
-	var module = angular.module('fontoxml-ui-special-characters', []);
+	var module = angular.module('fontoxml-ui-special-characters', [
+			uiButtonsUiModule,
+			uiModalUiModule,
+			uiRegionsUiModule,
+			uiPanelsUiModule
+		]);
 
 	module.controller('SpecialCharacterModalController', SpecialCharacterController);
 
