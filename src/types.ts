647ba97bdb7450286869319121c3cde5ea76fcbb
wvbe <wybe@x-54.com>
2021-11-17 12:50:18 +0100

    DEV-13017 Copy @property descriptions to doclets onto the properties

--- src/types.ts
+++ src/types.ts
@@ -33,18 +33,29 @@
  */
 export type CharacterSetEntry = {
 	/**
+	 * Unicode code points to insert when the character is inserted into the
+	 *                                      document, in order, encoded as strings starting with U+ followed by the
+	 *                                      hexadecimal representation of the code point.
+	 *
 	 * @fontosdk
 	 */
 	codePoints: string[];
 	/**
+	 * Unique identifier for the character.
+	 *
 	 * @fontosdk
 	 */
 	id: string;
 	/**
+	 * List of categories under which the character should appear in the modal,
+	 *                                      each specified using the descriptive label for the category shown in the modal.
+	 *
 	 * @fontosdk
 	 */
 	labels?: string[];
 	/**
+	 * Descriptive name for the character, displayed in the modal and as a tooltip content in the grid.
+	 *
 	 * @fontosdk
 	 */
 	name?: string;
